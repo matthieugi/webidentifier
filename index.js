0ee5312b1ea3074abfdbcc40609457a56dee5088
Matthieu Girard <matthieugirard@outlook.com>
2021-05-11 00:27:09 +0200

    Add Secret with managed identity

--- index.js
+++ index.js
@@ -1,22 +1,35 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const axios = require('axios');
 
 require('dotenv').config();
-const PORT = process.env.PORT;
 
-console.log(PORT);
+console.log(`Trying to start server on : ${process.env.PORT}`);
 
 app.use(morgan('tiny'));
 app.use(express.static('./public'));
 
 
-app.get('/color', (req, res) => {
+app.get('/color', async (req, res) => {
+  const accessToken = await axios.get('http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net', {
+    Headers: {
+      Metadata: 'true'
+    }
+  }).data.access_token;
+
+  const secret = await axios.get(`http://${process.env.VAULTURL}/secrets/${process.env.VAULTSECRETNAME}?api-version=2016-10-01`, {
+    Headers: {
+      Authorization: `Bearer ${accessToken}`
+    }
+  }).data.value
+
   res.send({
-    color: process.env.BGCOLOR
+    color: process.env.BGCOLOR,
+    secret: secret
   })
 });
 
-app.listen(PORT, () => {
-  console.log('server running on port : ' + PORT);
+app.listen(process.env.PORT, () => {
+  console.log('server running on port : ' + process.env.PORT);
 });
