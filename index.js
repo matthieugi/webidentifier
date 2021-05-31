55fe484d1807f5f19a2bd481833c385d102aecc8
Matthieu Girard <matthieugirard@outlook.com>
2021-05-31 19:56:09 +0200

    enhance token fetching

--- index.js
+++ index.js
@@ -10,23 +10,39 @@ console.log(`Trying to start server on : ${process.env.PORT}`);
 app.use(morgan('tiny'));
 app.use(express.static('./public'));
 
+app.get('/secret', async(req, res) => {
+  console.log(`Vault URL : ${process.env.VAULTURL}`);
+  console.log(`Secret Name : ${process.env.VAULTSECRETNAME}`);
 
-app.get('/color', async (req, res) => {
-  const accessToken = await axios.get('http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net', {
-    Headers: {
-      Metadata: 'true'
+  const metadataResponse = await axios.get('http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F', 
+    {
+      headers: {
+        Metadata: true
+      }
     }
-  }).data.access_token;
+  ).data;
 
-  const secret = await axios.get(`http://${process.env.VAULTURL}/secrets/${process.env.VAULTSECRETNAME}?api-version=2016-10-01`, {
+  console.log(`Fetched metadata endpoint : ${metadataResponse}`);
+  
+  const accessToken = metadataResponse.access_token;
+  
+  const vaultResponse = await axios.get(`http://${process.env.VAULTURL}/secrets/${process.env.VAULTSECRETNAME}?api-version=2016-10-01`, {
     Headers: {
       Authorization: `Bearer ${accessToken}`
     }
-  }).data.value
+  }).data
+  
+  console.log(`Fetched Key Vault endpoint : ${vaultResponse}`);
+  const secret = vaultResponse.value;
 
   res.send({
-    color: process.env.BGCOLOR,
     secret: secret
+  })
+});
+
+app.get('/color', async (req, res) => {
+  res.send({
+    color: process.env.BGCOLOR
   })
 });
 
