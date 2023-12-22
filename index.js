2d4e7f3945040b68c1fbb8d87e86e02216a2f415
matthieugirard <matthieugirard@outlook.com>
2023-12-22 16:19:27 +0100

    add simple return requested values

--- index.js
+++ index.js
@@ -8,12 +8,32 @@ console.log(`Trying to start server on : ${process.env.PORT}`);
 
 app.use(morgan('tiny'));
 app.use(express.static('./public'));
+app.use(express.json())
 
 app.get('/color', async (req, res) => {
   res.send({
     color: process.env.BGCOLOR
   })
 });
+
+app.get('*', async (req, res) => {
+  res.send({
+    path: req.url,
+    color: process.env.BGCOLOR
+  })
+});
+
+app.post('*', async (req, res) => {
+  console.log(req.body);
+
+  res.send({
+    path: req.url,
+    color: process.env.BGCOLOR,
+    ...req.body
+  })
+});
+
+
 
 app.listen(process.env.PORT, () => {
   console.log('server running on port : ' + process.env.PORT);
