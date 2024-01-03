a71f45679029a3fb5a23084b1e55a69ddc9c54ee
matthieugirard <matthieugirard@outlook.com>
2024-01-03 15:21:28 +0100

    fix env variables

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const morgan = require('morgan');
 
 require('dotenv').config();
+const COLOR = process.env.COLOR || "red";
+const PORT = process.env.PORT || 3000;
 
-console.log(`Trying to start server on : ${process.env.PORT}`);
+console.log(`Trying to start server on : ${PORT}`);
 
 app.use(morgan('tiny'));
 app.use(express.static('./public'));
@@ -12,14 +14,14 @@ app.use(express.json())
 
 app.get('/color', async (req, res) => {
   res.send({
-    color: process.env.BGCOLOR
+    color: COLOR
   })
 });
 
 app.get('*', async (req, res) => {
   res.send({
     path: req.url,
-    color: process.env.BGCOLOR
+    color: COLOR
   })
 });
 
@@ -28,13 +30,11 @@ app.post('*', async (req, res) => {
 
   res.send({
     path: req.url,
-    color: process.env.BGCOLOR,
+    color: COLOR,
     ...req.body
   })
 });
 
-
-
 app.listen(process.env.PORT, () => {
-  console.log('server running on port : ' + process.env.PORT);
+  console.log('server running on port : ' + PORT);
 });
