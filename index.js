a8a6a275e3198a9948e2d3119bac2a654e904ebe
matthieugirard <matthieugirard@outlook.com>
2024-01-04 10:14:00 +0100

    add policy generator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const { randomUUID } = require('crypto');
 
 const COLOR = process.env.BGCOLOR || "blue";
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 console.log(`Trying to start server on : ${PORT}`);
 console.log(`Color is : ${COLOR}`);
 
+let policyIds = [];
 
 app.use(morgan('tiny'));
 app.use(express.static('./public'));
@@ -16,6 +18,38 @@ app.use(express.json())
 app.get('/color', async (req, res) => {
   res.send({
     color: COLOR
+  })
+});
+
+app.get('/policy/:id', async (req, res) => {
+  const id = req.params.id;
+
+  if (policyIds.includes(id)) {
+    res.send({
+      id: id,
+      path: req.url,
+      color: COLOR
+    })
+  } else {
+    res.status(404).send({
+      id: id,
+      path: req.url,
+      color: COLOR,
+      error: "Not Found"
+    })
+  }
+});
+
+app.post('/policy', async (req, res) => {
+  const id = randomUUID();
+  policyIds.push(id);
+
+
+  res.send({
+    id: id,
+    path: req.url,
+    color: COLOR,
+    ...req.body
   })
 });
 
